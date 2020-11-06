b50524f6694b5bb5fb9a88afd2d865115dfc3f67
n0mine <ajtajaja13@gmail.com>
2020-11-06 22:28:33 +0800

    added new user control

---
+++ app/add task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace app
+{
+    public partial class add_task : UserControl
+    {
+        public add_task()
+        {
+            InitializeComponent();
+        }
+    }
+}
