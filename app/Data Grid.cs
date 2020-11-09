4b8482ddbfd09fffc208003c17d3b871bfd85c56
n0mine <ajtajaja13@gmail.com>
2020-11-09 18:57:08 +0800

    fixed empty text file error

--- app/Data Grid.cs
+++ app/Data Grid.cs
@@ -48,12 +48,19 @@ namespace app
         {
             table.Clear();
             string[] lines = getFile();
-            string[] elements; // get the elements from a line;
-            for (int i = 0; i < lines.Length; i++) {
-                elements = lines[i].Split(x, StringSplitOptions.None); //splits the line into elements and stores it
-                //makes the date to a short one - Nov/08/2020
-                elements[2] = DateTime.Parse(elements[2]).ToString("MMM/dd/yyyy");
-                table.Rows.Add(elements); //adds element to a row
+            if (lines.Length < 1)
+            {
+                table.Rows.Add("you", "take a break", "for", "now", "false");
+            }
+            else {
+                string[] elements; // get the elements from a line;
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    elements = lines[i].Split(x, StringSplitOptions.None); //splits the line into elements and stores it
+                                                                           //makes the date to a short one - Nov/08/2020
+                    elements[2] = DateTime.Parse(elements[2]).ToString("MMM/dd/yyyy");
+                    table.Rows.Add(elements); //adds element to a row
+                }
             }
         }
         public string[] getFile()
@@ -64,29 +71,31 @@ namespace app
         public void update() {
             List<string> temp = new List<string>();
             temp = File.ReadAllLines(path).ToList();
-            List<int> checkedMark = new List<int>();
-            int numRows = ((DataTable)this.dataGrid.DataSource).Rows.Count;
-            for (int i = 0; i < numRows; i++)
-            {
-                //checks the values of every row under "status"
-                if (dataGrid.Rows[i].Cells[4].Value.ToString() == "True")
+            if (temp.Count() > 0) {
+                List<int> checkedMark = new List<int>();
+                int numRows = ((DataTable)this.dataGrid.DataSource).Rows.Count;
+                for (int i = 0; i < numRows; i++)
                 {
-                    checkedMark.Add(i);
+                    //checks the values of every row under "status"
+                    if (dataGrid.Rows[i].Cells[4].Value.ToString() == "True")
+                    {
+                        checkedMark.Add(i);
+                    }
+
                 }
 
+                checkedMark.Sort();
+                for (int j = checkedMark.Count() - 1; j >= 0; j--)
+                {
+                    temp.RemoveAt(checkedMark[j]);
+                }
+                File.WriteAllText(path, String.Empty);
+                File.WriteAllLines(path, temp);
+                getFile();
+                checkedMark.Clear();
             }
-
-            checkedMark.Sort();
-            for (int j = checkedMark.Count() - 1; j >= 0; j--)
-            {
-                temp.RemoveAt(checkedMark[j]);
-            }
-            File.WriteAllText(path, String.Empty);
-            File.WriteAllLines(path, temp);
-            getFile();
             showData();
             temp.Clear();
-            checkedMark.Clear();
         }
     }
 }
