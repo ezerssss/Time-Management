0f632847b850ac152353d1a48f7df397843d0779
n0mine <ajtajaja13@gmail.com>
2020-11-14 22:27:08 +0800

    minimize button

--- app/Form1.cs
+++ app/Form1.cs
@@ -152,5 +152,10 @@ namespace app
         {
             this.Close();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
     }
 }
